c3f9f1de58c80906890598d0ebd6077746150c3e
Gabriel R. <junior.gibson.lp@gmail.com>
2024-07-18 17:23:13 -0600

    Modal Edit Agregado. Sin funcionalidad aun.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     <h1 class="text-center p-3">CRUD Laravel </h1>
 
     <div class="p-5 table-responsive">
-        <table class="table">
+        <table class="table table'striped">
             <thead>
                 <tr>
                     <th scope="col">Código (ID)</th>
@@ -27,10 +27,33 @@
                         <td>{{$item->product_name}}</td>
                         <td>${{$item->price}}</td>
                         <td>{{$item->stock}}</td>
+                        <td>
+                            <a href="#" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"> Editar </a>
+                            <a href="#" class="btn btn-danger btn-sm"> Eliminar</a>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
